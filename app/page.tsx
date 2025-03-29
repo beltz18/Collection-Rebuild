499bf4350e1caee3dd4d213671adfbda2b89dbf8
Andi Montilla <anmdev32@gmail.com>
2025-03-29 17:15:29 -0300

    breadcrumbs component

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,16 @@
 'use client'
 
-import { Sidebar } from '@sec/index'
+import { Breadcrumbs } from "@com/breadcrumbs"
+import { Home, Folder, FileText } from "lucide-react"
 
-export default function Home() {
+const navItems = [
+  { label: "Inicio", href: "#home", icon: Home },
+  { label: "Proyectos", href: "#proyectos", icon: Folder },
+  { label: "Detalles", href: "#detalles", icon: FileText }
+]
+
+export default function App() {
   return (
-    <Sidebar>
-      <Sidebar.Toggle />
-      <Sidebar.Content>
-        <Sidebar.Header>Head</Sidebar.Header>
-
-        <Sidebar.Body>
-          <Sidebar.Item>Home</Sidebar.Item>
-          <Sidebar.Item>About</Sidebar.Item>
-          <Sidebar.Item>Contact</Sidebar.Item>
-        </Sidebar.Body>
-
-        <Sidebar.Footer>Logout</Sidebar.Footer>
-      </Sidebar.Content>
-    </Sidebar>
+    <Breadcrumbs items={navItems} className="text-gray-700" />
   )
 }
