aaff27b36745e3a254f81136b09a2ab53d9bffce
Andi Montilla <anmdev32@gmail.com>
2025-03-29 17:15:42 -0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,19 @@ export default function Home() {
     <Sidebar>
       <Sidebar.Toggle />
       <Sidebar.Content>
-        <Sidebar.Header>Head</Sidebar.Header>
-
+        <Sidebar.Header>
+          Head
+        </Sidebar.Header>
+        
         <Sidebar.Body>
           <Sidebar.Item active>Home</Sidebar.Item>
           <Sidebar.Item>About</Sidebar.Item>
           <Sidebar.Item>Contact</Sidebar.Item>
         </Sidebar.Body>
-
-        <Sidebar.Footer>Logout</Sidebar.Footer>
+        
+        <Sidebar.Footer>
+          Logout
+        </Sidebar.Footer>
       </Sidebar.Content>
     </Sidebar>
   )
