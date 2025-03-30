92c6853e3d7498e6fcf58dfbb6c3aa33d1f24519
Andi Montilla <anmdev32@gmail.com>
2025-03-29 23:35:54 -0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 'use client'
 
+import { DefaultLayout } from '@lay/default'
+
 export default function App() {
   return (
-    <>
-    </>
+    <DefaultLayout>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Rerum doloremque amet delectus blanditiis vitae modi, qui
+        ipsam nisi nam optio odio expedita odit nesciunt, iste
+        possimus? Dolorem reprehenderit provident facilis.
+      </p>
+    </DefaultLayout>
   )
 }
