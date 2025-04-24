a53b3fd3c0cb8c6b42e9410f468275428532d994
Andi Montilla <anmdev32@gmail.com>
2025-04-24 13:11:04 -0300

    Changes to the /payments/steps view

--- app/(auth)/payments/steps/component-container.tsx
+++ app/(auth)/payments/steps/component-container.tsx
@@ -1,6 +1,7 @@
 import { Card } from '@com/card/card-loan'
 import { Button, Icon } from '@com/index'
 import { CustomSelect } from '@com/select/select'
+import { Heading } from '@com/index'
 
 type Props = {
   data: string[]
@@ -8,15 +9,17 @@ type Props = {
 export const ComponentContainer = ({ data }: Props) => {
 
   return (
-    <Card className='p-2 w-full min-w-xl max-w-xl'>
+    <Card className='p-2 w-full min-w-xl max-w-xl rounded-sm'>
       <Card.Header className='flex justify-center items-center'>
-        <p className='font-bold'>Select Payment Strategy</p>
+        <Heading level={1} className='font-bold text-xl'>Select Payment Strategy</Heading>
       </Card.Header>
       <Card.Footer className='flex flex-col'>
         <div className='flex flex-col space-y-3 w-full'>
-          <span>Payment Strategy</span>
-          <CustomSelect variant='bordered' className='w-full' itemsSelect={data} label='Select a strategy' />
-          <Button color='primary' placeholder='Continue to Flow Chart' endContent={<Icon icon='arrowRight' />} className='w-full' />
+          <div className='pb-4'>
+            <label className='text-sm font-medium block mb-2'>Payment Strategy</label>
+            <CustomSelect variant='faded' radius='sm' className='w-full' itemsSelect={data} label='Select a strategy' />
+          </div>
+          <Button color='primary' placeholder='Continue to Flow Chart' endContent={<Icon icon='arrowRight' />} className='w-full rounded-sm' />
         </div>
       </Card.Footer>
     </Card>
