4cba83570986c1fdf2185ca35de0b2976268fdc1
Andi Montilla <anmdev32@gmail.com>
2025-04-27 15:03:11 -0300

    fixes

--- components/common/select/select.tsx
+++ components/common/select/select.tsx
@@ -1,17 +1,28 @@
 'use client'
 
-import { Select, SelectItem } from '@heroui/react'
+import {
+  Select,
+  SelectItem,
+} from '@heroui/react'
 import React from 'react'
 import { SelectProps } from './select.types'
 
-export const CustomSelect = ({ className, label, itemsSelect,
-  ...props }: SelectProps) => {
+export const CustomSelect = ({
+  className,
+  label,
+  itemsSelect,
+  ...props
+}: SelectProps) => {
   return (
     itemsSelect && (
       <Select className={className} label={label} {...props}>
-        {itemsSelect.map((item, index) => (
-          <SelectItem key={index}>{item}</SelectItem>
-        ))}
+        {
+          itemsSelect.map((item, index) => 
+            <SelectItem key={ index }>
+              { item }
+            </SelectItem>
+          )
+        }
       </Select>
     )
   )
